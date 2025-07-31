16213caa9d843645aaf104a3a8b9e946fc54c353
Wolfram121 <avi.baj@outlook.com>
2025-07-31 19:38:41 -0400

    Successful Communications Test with Client Code. >>> github.com:Wolfram121/WolfRecieve9289-2.git

--- src/main/java/frc/robot/subsystems/WolfSend.java
+++ src/main/java/frc/robot/subsystems/WolfSend.java
@@ -17,6 +17,9 @@ public class WolfSend extends SubsystemBase {
     private final NetworkTableEntry LBR;
     private final NetworkTableEntry RBR;
     private final NetworkTableEntry RFR;
+    private final NetworkTableEntry POS_X;
+    private final NetworkTableEntry POS_Y;
+    private final NetworkTableEntry ANGLE;
     private final NetworkTableEntry statusText;
 
     private Drivetrain drivetrain = Drivetrain.getInstance();
@@ -35,6 +38,9 @@ public class WolfSend extends SubsystemBase {
         LBR = t.getEntry("LBR");
         RBR = t.getEntry("RBR");
         RFR = t.getEntry("RFR");
+        POS_X = t.getEntry("POS_X");
+        POS_Y = t.getEntry("POS_Y");
+        ANGLE = t.getEntry("ANGLE");
         statusText = t.getEntry("status");
     }
 
@@ -50,7 +56,9 @@ public class WolfSend extends SubsystemBase {
         LBR.setDouble(positions[2].angle.getDegrees());
         RBR.setDouble(positions[3].angle.getDegrees());
         RFR.setDouble(positions[1].angle.getDegrees());
-        statusText.setString("System nominal");
+        POS_X.setDouble(drivetrain.getPose().getX());
+        POS_Y.setDouble(drivetrain.getPose().getY());
+        statusText.setString("Uknown Status");
     }
 
     public static WolfSend getInstance() {
